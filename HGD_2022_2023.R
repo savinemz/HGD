197ce91a604a25f75b6562704feacf724619a753
savinemz <savine.mermilliod@orange.fr>
2023-02-15 23:07:09 +0100

    nettoyage

--- HGD_2022_2023.R
+++ HGD_2022_2023.R
@@ -2,3 +2,27 @@
 #Schema de dispersion postnatal du HGD en region HDF
 
 setwd("C:/Git/HGD")
+
+library(data.table)
+library(ggplot2)
+
+data_HGD <- read.csv2("data_HGD.csv", head = T, sep = ";")
+data_HGD <- subset(data_HGD, Affichage == "OUI")
+
+attach(r)
+summary(r)
+shapiro.test(r$X2001)
+shapiro.test(r$X2005)
+boxplot(r, notch = T)
+
+library(car)
+library(faraway)
+library(hier.part)
+library(MASS)
+library(mgcv)
+library(MuMIn)
+library(multcomp)
+library(shiny)
+library(vegan)
+library(xtable)
+library(glmmTMB)
