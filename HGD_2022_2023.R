159f200789103153e5a6e03810a0f48884aa901e
Savine Mermilliod <savine.mermilliod@orange.fr>
2023-02-28 17:04:55 +0100

    Intersection HGD_habitat

--- HGD_2022_2023.R
+++ HGD_2022_2023.R
@@ -14,12 +14,26 @@ library(shiny)
 library(vegan)
 library(xtable)
 library(glmmTMB)
-library(dplyr)
 
 
 setwd("C:/Git/HGD")
 
 
+### Data Habitat ####
+habitat <- st_read("SIG/CLC_HDF_2018.shx", stringsAsFactors = T)
+habitat <- habitat[,-4]
+summary(habitat)
+habitat <- st_transform(habitat,crs=3832)
+
+gg <- plot(code_18)
+ggsave("Rplot/hab.png",gg)#marche pas
+
+habitat$area_poly <- st_area(habitat)
+
+
+
+
+### Data HGD ####
 data_HGD_original <- read.csv2("data_HGD.csv", head = T, sep = ";", stringsAsFactors = T)
 plot(data_HGD_original$Affichage)
 summary(data_HGD_original)
@@ -29,28 +43,18 @@ summary(data_HGD)
 
 data_HGD <- subset(data_HGD, Commentair != "Pas de coordonnées GPS")
 summary(data_HGD)
-attach(data_HGD)
 
-
-
-habitat <- st_read("SIG/CLC_HDF_2018.shx")
-habitat <- habitat[,-4]
-habitat$area_poly <- st_area(habitat)
-
-
-
-
-#supression colonnes
+#Nettoyage des données
 data_HGD <- data_HGD[,-c(1,5,27,28)]#device_id, datatype, Affichage, Commentair
 summary(data_HGD)
 names(data_HGD)[25] <- "bird_id"
 data_HGD <- data_HGD %>% relocate(bird_id, .after = UTC_dateti)
 data_HGD <- data_HGD %>% relocate(UTC_dateti, .after = bird_id)
 summary(data_HGD)
-
+attach(data_HGD)
 
 plot(bird_id)
-boxplot(Altitude_m~bird_id, notch = T)
+#boxplot(Altitude_m~bird_id, notch = T)
 
 ## ajout de la colonne date et la colonne heure manuellement
 data_HGD$heure_HH <- substr(data_HGD$UTC_time,1,2)
@@ -61,9 +65,6 @@ HGD_sf <- st_as_sf(data_HGD, coords = c("Longitude","Latitude"))
 st_crs(HGD_sf) <- 4326
 HGD_sf <- st_transform(HGD_sf,crs=3832)
 sum_loc <- st_intersection(habitat, HGD_sf)
-
-
-
 
 
 setDT(data_HGD)
@@ -78,7 +79,21 @@ gg <- ggplot(nb_data_HH,aes(x=nb_data_HH,y=bird_id)) + geom_violin()
 gg <- gg + labs(y = "Bird_id", x = "Number of data per hour")
 gg
 
-ggsave("Rplot/nb_data_HH.png",gg)
+ggsave("Rplot/nb_data_HH2.png",gg)
 
 gg2 <- ggplot(nb_data_HH,aes(x=nb_data_HH)) + geom_histogram() + facet_grid(bird_id~.)
 gg2
+
+
+
+#calcul des surfaces totales par habitat
+area_hab <- aggregate(area_poly~code_18, habitat, sum)
+
+
+#calcul des surfaces par motu
+area_tot <- aggregate(habitat$area_poly, habitat, sum)
+names(area_motu)[2] <- "area_motu"
+rangi <- merge(rangi, area_motu, by = "id_motu")
+
+
+
